0d22b4209046ded31bc9fd6c39fc1d01b4e494d8
rahulyadav <rahulyadav170923@gmail.com>
2018-05-06 04:58:32 +0530

    Binding a port

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
 import os
 # putting configuring options here
 secret_key = os.environ['secret']
-try:
-    port = os.environ['PORT']
-except KeyError:
-    port = 3000
+port = int(os.environ.get('PORT', 5000))
